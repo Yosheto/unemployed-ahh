15dbf29ad4fb2995fb8f606e3d8ef8ea1c0c2247
the_hongler <Agari15624@yandex.ru>
2025-12-03 09:59:24 +0300

    Create Individual task 4.cpp

---
+++ Individual task 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//Программа, которая нечётное число заменяет на 0,а чётное число уменьшает в 2 раза
+int f(int x)
+{
+    return x % 2 ? 0 : x / 2;
+}
+int main()
+{
+    //Пример работы программы
+    cout << "f(10) = " << f(10) << endl;
+    cout << "f(5) = " << f(5) << endl;
+    return 0;
+}
