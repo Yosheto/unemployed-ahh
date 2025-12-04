9167bc149c5a5d88b4654e20ff96a454c94b5a58
the_hongler <Agari15624@yandex.ru>
2025-12-05 02:40:55 +0300

    Update individual task 1.cpp

--- individual task 1.cpp
+++ individual task 1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>   //Библиотека ввода-вывода
 #include <cmath>      //Математические функции
-
+#include <iomanip>    //Библиотека манипуляторов
 using namespace std;
 
 int main()
@@ -13,9 +13,10 @@ int main()
     (x - 1 < 0) && (cout << "Ошибка: выражение под вторым корнем отрицательное") << endl;
     //Вычисление выражения
     double result = sin(sqrt(x + 1)) - sin(sqrt(x - 1));
-    //Вывод результата с точностью до 4 знаков после запятой
-    cout.precision(4);
+    //Вывод результата с 4 знаками после запятой
+    cout << setprecision(4);
     cout << "Результат выражения sin√(x+1) - sin√(x-1) = " << fixed << result << endl;
     
     return 0;
 }
+
